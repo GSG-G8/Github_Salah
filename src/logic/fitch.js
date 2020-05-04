7479a12e590dc5bdca1609fe14e5021f9aa86c5b
ahmadysalah <a.ahmad.salah@gmail.com>
2020-05-04 18:24:07 +0300

    fix env file

--- src/logic/fitch.js
+++ src/logic/fitch.js
@@ -1,15 +1,12 @@
-import accessToken from '../token';
-
 const checkResponse = response => {
   if (response.status !== 200) {
-    console.log(`Error with the request! ${response.status}`);
-    return;
+    return response.json(`Error with the request! ${response.status}`);
   }
   return response.json();
 };
 
 const getUserData = url => {
-  return fetch(`${url}?access_token=${accessToken}`)
+  return fetch(`${url}?access_token=${process.env.accessToken}`)
     .then(checkResponse)
     .catch(err => {
       throw new Error(`fetch getUserData failed ${err}`);
